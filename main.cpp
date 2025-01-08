11af127393c8af62e46bc09a310f128b9b6664f5
Vlad <vladutiku@gmail,com>
2025-01-08 03:01:31 +0200

    test

--- main.cpp
+++ main.cpp
@@ -2,8 +2,10 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include "include/Game.h"
-
+#include "src/Game.cpp"
+#include "src/Player.cpp"
+#include "src/Utilities.cpp"
+#include "src/Card.cpp"
 
 
 int main()
